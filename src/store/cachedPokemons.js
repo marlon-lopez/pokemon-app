7ba79079975e851f22fac44d8314b9953584b18d
marlon-lopez <marlonlopez5647@gmail.com>
2021-03-24 23:27:09 -0600

    search pokemon finished

--- src/store/cachedPokemons.js
+++ src/store/cachedPokemons.js
@@ -5,6 +5,7 @@ const slice = createSlice({
   name: 'cachedPokemons',
   initialState: {
     cache: [],
+    search: [],
     loading: false,
   },
   reducers: {
@@ -15,16 +16,33 @@ const slice = createSlice({
       state.cache = action.payload
       slice.caseReducers.toggleLoading(state)
     },
+    searchedPokemons(state, action) {
+      state.search = action.payload
+    },
+    resetSearch(state) {
+      state.search = []
+    },
   },
 })
 
-export const { toggleLoading, pokemonsCached } = slice.actions
+export const {
+  toggleLoading,
+  pokemonsCached,
+  searchedPokemons,
+  resetSearch,
+} = slice.actions
 
 export const getCachedPokemons = () => async (dispatch, getState) => {
   const { results } = await getPokemons('809')
   dispatch(toggleLoading())
 
   dispatch(pokemonsCached(results))
+}
+
+export const searchPokemonsByName = (name) => (dispatch, getState) => {
+  const { cache } = getState().cachedPokemons
+  const result = cache.filter((pokemon) => pokemon.name.includes(name))
+  dispatch(searchedPokemons(result))
 }
 
 export default slice.reducer
